d0008059fd6af89ae27de8afd066eb631130ef8a
sims-bit <xfiqoe46@students.codeinstitute.net>
2025-02-03 09:02:32 +0000

    Changed the chickenDinner function return to string returns.  Removed while loop. Introduced Game rounds.

--- assets/Javascript.js
+++ assets/Javascript.js
@@ -1,57 +1,57 @@
 //Play button initiates transition
  
-
-
-
-
 const gameOptions = ['knifey', 'spooney', 'spork'];
 
 //Comparison function
 function chickenDinner (humanInput, computerInput){
     if(humanInput == computerInput){
-        return true;  /*REMEMBER TO CHANGE THIS BACK*/
+        return 'tie';  /*REMEMBER TO CHANGE THIS BACK*/
     } else if(humanInput == 'knifey'){
     if(computerInput == 'spooney'){
-        return true;
+        return 'human';
     } else {
         if(computerInput == 'spork'){
-            return false;
+            return 'computer';
         }
     }
 }
 if(humanInput == 'spooney') {
     if(computerInput == 'spork'){
-        return true;
+        return 'human';
     } else {
         if(computerInput == 'knifey'){
-            return false;
+            return 'computer';
         }
     }
 }
 if(humanInput == 'spork'){
     if(computerInput == 'knifey'){
-        return true;
+        return 'human';
     } else {
         if(computerInput == 'spooney'){
-            return false;
+            return 'computer';
         }
     }
 }
 }
+
+
+
 //incrementing while loop
 let humanInputWinsPoint = 0; 
 let computerInputWinsPoint = 0;
- 
-while(humanInputWinsPoint + computerInputWinsPoint < 3){
-  let humanInput = ['Knifey', 'Spooney', 'Spork'];
-  let computerInput = gameOptions[Math.floor(Math.random()*gameOptions.length)];
-  let ret = chickenDinner(humanInput, computerInput);
-if(ret){
-  humanInputWinsPoint++
-  } else {
-  computerInputWinsPoint++}
-}
-// Event handler for play buttons. Triggers a turn - gets the computer's choice and then calls the comparison function
+let gameRounds = 0;
+
+// while(humanInputWinsPoint + computerInputWinsPoint < 3){
+//   let humanInput = ['Knifey', 'Spooney', 'Spork'];
+//   let computerInput = gameOptions[Math.floor(Math.random()*gameOptions.length)];
+//   let ret = chickenDinner(humanInput, computerInput);
+
+// }
+
+// *Event handler for play buttons. Triggers a turn from users click selection. Gets the computer's choice using math.random one-liner. 
+// calls the chickenDinner comparison function
+
 function playTurn(event){
     const clickedButton = event.currentTarget;
     let humanInput;
@@ -62,8 +62,28 @@ function playTurn(event){
     } else {
         humanInput = 'spork';
     }
+    let computerInput = gameOptions[Math.floor(Math.random()*gameOptions.length)];
+    
+    let ret = chickenDinner(humanInput, computerInput);
+
+    if (ret == 'human') {
+        humanInputWinsPoint++;
+        console.log('human');
+    } else if (ret == 'computer') {
+        computerInputWinsPoint++;
+        console.log('computer');
+    } 
+    gameRounds++;
+    if( gameRounds >= 3){
+        if( humanInputWinsPoint > computerInputWinsPoint){
+            console.log("AHHH I see you've played knifey spooney before mate!");
+        } else if( humanInputWinsPoint < computerInputWinsPoint) {
+            console.log("Thats not a knife! This is a knife!");
+        }
+    }
 }
-document.getElementById("knifey-button").addEventListener("click", console.log('fs'));
+
+const knifeyButton = document.getElementById("knifey-button").addEventListener("click", playTurn);
 const spooneyButton = document.getElementById("spooney-button");
 const sporkButton = document.getElementById('spork-button');
 
@@ -71,23 +91,29 @@ const sporkButton = document.getElementById('spork-button');
 spooneyButton.addEventListener('click', playTurn);
 sporkButton.addEventListener('click', playTurn);
 
-//Winner/looser end message 
-if( humanInputWinsPoint > computerInputWinsPoint){
-    console.log("AHHH I see you've played knifey spooney before mate!");
-} else{( humanInputWinsPoint < computerInputWinsPoint)
-    console.log("Thats not a knife! This is a knife!");
-}
+
+
+// //Winner/looser end message 
+// if( humanInputWinsPoint > computerInputWinsPoint){
+//     console.log("AHHH I see you've played knifey spooney before mate!");
+// } else{( humanInputWinsPoint < computerInputWinsPoint)
+//     console.log("Thats not a knife! This is a knife!");
+// }
 
 //exit button/ function
 document.getElementById("exit-button").addEventListener("click", exitButton);
 
 function exitButton(){
-    let humanInputWinsPoint = 0;
-    let computerInputWinsPoint = 0;
+    humanInputWinsPoint = 0;
+    computerInputWinsPoint = 0;
+    gameRounds = 0;
     console.log(humanInputWinsPoint, computerInputWinsPoint);
+    
     }
 
 //display computer option
+
+
 
 
 
